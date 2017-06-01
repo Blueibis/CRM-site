85864f4ca77d68c3c8507e8c0d05bcf902691714
Blueibis <jennie.li.2011@gmail.com>
2017-06-01 16:56:01 -0400

    completed site for now

--- crm.rb
+++ crm.rb
@@ -1,6 +1,22 @@
 require 'sinatra'
+require_relative 'note'
 require_relative 'contact'
+require 'pry'
+
 
 get '/' do
   erb :index
+end
+
+get '/contacts' do
+  @contacts = Contact.all
+  # @contact_note = Note.all
+  erb :contacts
+end
+
+get '/about_me' do
+  erb :about
+end
+after do
+  ActiveRecord::Base.connection.close
 end
---
+++ note.rb
@@ -0,0 +1,12 @@
+require 'active_record'
+require 'mini_record'
+
+ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'crm.sqlite3')
+
+class Note < ActiveRecord::Base
+  field :contact_id, as: :integer
+  field :body, as: :string
+  field :written_at, as: :datetime
+end
+
+Note.auto_upgrade!
